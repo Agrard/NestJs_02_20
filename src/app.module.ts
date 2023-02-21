624936fde274dcd5879ab500f288072aa00b061b
Agrard <gabor.kallai12@gmail.com>
2023-02-21 13:28:06 +0100

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { StaticModule } from './Static/static.module';
-import { StaticService } from './Static/static.service';
 import { TodoModule } from './Todo/TodoModule';
-import { TodoService } from './Todo/TodoService';
 
 @Module({
   imports: [TodoModule,StaticModule],
